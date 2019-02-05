6d25e2f86ebd4d1cefb8c445b46cfacc7d860c88
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-05 03:48:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 # Add  code here!
-def prime? (n)
- for d in 2..(n - 1)
-  if (n % d) == 0
-   return false
-  end
- end
+def prime?(n)
+  if n <= 1
+  return false
 
- true
+  else (2..n-1).to_a.all? do |integer|
+   n % integer != 0
+
+   end
+  end
 end
